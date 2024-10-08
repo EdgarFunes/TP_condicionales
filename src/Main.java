59707ef6c5a9ac0d40651855ff7f9a8d250d81d8
EdgarFunes <edgardoibarfunesortiz@gmail.com>
2024-10-08 00:59:49 -0300

    ejercicio 2

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,11 @@ public class Main {
         int option = 0;
         try{
             do {
-                System.out.println("""
-                        ¿Que elercicio desea ejecutar?
+                System.out.print("""
+                        ¿Que ejercicio desea ejecutar?
                         0. Salir
                         1. Sistema de recomendación de peliculas
+                        2. Calculadora de Descuentos
                         """);
                 option = scanner.nextInt();
 
@@ -21,10 +22,12 @@ public class Main {
                     case 2:
                         ejercicio2(scanner);
                         break;
+                    default:
+                        System.out.println("Finalizando ejecucion");
                 }
             }while (option != 0);
         }catch(Exception e){
-
+            System.out.println("Error: "+e.getMessage());
         }
     }
     public static void ejercicio1(Scanner scanner){
@@ -46,14 +49,36 @@ public class Main {
                     2. Comedia
                     3. Drama
                     4. Ciencia Ficcion
-                    Ingrese su respuesta:
                     """);
             option = scanner.nextInt();
             if(option < 1 || option > 4) throw new Exception("Opcion no valida");
             else System.out.println("Le recomendamos: "+peliculas[option-1]);
         }catch(Exception e){
-            System.out.println("Error: "+e);
+            System.out.println("Error: "+e.getMessage());
         }
     }
-    public static void ejercicio2(Scanner scanner){}
+    public static void ejercicio2(Scanner scanner){
+        System.out.println("Ejercicio 2: Calculadora de Descuentos");
+        /*Escribe un programa que pida al usuario el precio de un producto y la categoría del cliente
+        (estudiante, adulto, jubilado). Aplica un descuento del 10% para estudiantes, 5% para adultos y
+        15% para jubilados. Imprime el precio final después del descuento*/
+
+        float[] descuentos = {0.1f, 0.05f, 0.15f};
+        try{
+            System.out.print("Escribe el precio del producto: ");
+            float precio = scanner.nextFloat();
+
+            System.out.print("""
+                    Escribe la categoria a la pertenece:
+                    1. Estudiante
+                    2. Adulto
+                    3. Jubilado
+                    """);
+            int categoria = scanner.nextInt();
+            if(categoria > 3 && categoria < 1) throw new Exception("Opcion no valida");
+            else System.out.println("El precio final es: "+(precio*(1-descuentos[categoria-1])));
+        }catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
 }
